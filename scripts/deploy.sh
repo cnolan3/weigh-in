3c561466fcdb964c3eba55c34b70dbb038fa757c
Connor Nolan <connornolan15@gmail.com>
2018-05-17 22:58:50 -0700

    edit deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,14 +3,16 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo, and push it to our server.
+	mkdir angular-build
+	mv angular-src/dist/* angular-build/
+
     git init
         
     git remote add deploy "deploy@weighindebates.com:/var/www/wi"
     git config user.name "Travis CI"
     git config user.email "connor.nolan+travisCI@gmail.com"
     
-    git add ./backend 
-	git add -f ./angular-src/dist
+    git add ./backend ./angular-build
     git commit -m "Deploy"
     git push --force deploy master
 else
