886372a8b39fce62acd5627935b7a525bbbd294c
Connor Nolan <connornolan15@gmail.com>
2018-05-17 21:39:01 -0700

    edit travis config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+# adapted from https://kjaer.io/travis/ 
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-    # Initialize a new git repo in _site, and push it to our server.
-    cd _site
+    # Initialize a new git repo, and push it to our server.
     git init
         
     git remote add deploy "deploy@weighindebates.com:/var/www/wi"
     git config user.name "Travis CI"
     git config user.email "connor.nolan+travisCI@gmail.com"
     
-    git add .
+    git add ./backend ./angular-src/dist
     git commit -m "Deploy"
     git push --force deploy master
 else
