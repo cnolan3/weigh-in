fb0f2952362f0e0c35afe22c123057cb42ce2809
Connor Nolan <connornolan15@gmail.com>
2018-05-18 02:30:31 -0700

    fix syntax error

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -52,7 +52,7 @@ router.post('/register', (req, res, next) => {
 	/// check for email
 	/// adapted from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	if(!re.test(String(req.body.email).toLowerCase()) 
+	if(!re.test(String(req.body.email).toLowerCase()))
 		return res.status(400).send('InvalidEmail');
 
 	/// check for complete info
